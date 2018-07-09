a2d5458a31db0380248e7a8166a8d235c71df3f1
Ofir Ben Shahar <oophir1@gmail.com>
2018-07-09 12:01:23 +0300

    Added the beer class

---
+++ app/src/main/java/ory/ofir/beerz/Model/Beer.java
@@ -0,0 +1,54 @@
+package ory.ofir.beerz.Model;
+
+
+import android.support.annotation.NonNull;
+import android.arch.persistence.room.PrimaryKey;
+import android.arch.persistence.room.Entity;
+
+@Entity
+public class Beer {
+    @PrimaryKey
+    @NonNull
+    public String id;
+    public String name;
+    public String picture;
+    public float rating;
+    public String description;
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
