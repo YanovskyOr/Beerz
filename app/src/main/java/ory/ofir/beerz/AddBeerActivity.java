d038f5288c4be9ebb6efac755ecfdd68e02a3da4
YanovskyOr <yanovsky.or@gmail.com>
2018-07-11 23:24:16 +0300

    added auth for add beer activity

--- app/src/main/java/ory/ofir/beerz/AddBeerActivity.java
+++ app/src/main/java/ory/ofir/beerz/AddBeerActivity.java
@@ -1,10 +1,17 @@
 package ory.ofir.beerz;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AddBeerActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,16 @@ public class AddBeerActivity extends AppCompatActivity {
         FragmentTransaction tran = getSupportFragmentManager().beginTransaction();
         tran.replace(R.id.addBeerContainer, fragment);
         tran.commit();
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        Log.d("tag", "currentUser is :" +currentUser);
+        if(currentUser == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+            startActivity(intent);
+            this.finish();
+        }
     }
 }
--- app/src/main/java/ory/ofir/beerz/Model/Beer.java
+++ app/src/main/java/ory/ofir/beerz/Model/Beer.java
@@ -28,8 +28,6 @@ public class Beer {
         return name;
     }
 
-    public String getName() { return name; }
-
     public void setName(String name) {
         this.name = name;
     }
