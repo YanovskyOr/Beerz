e74eb9720c7387abeefaac25fdabe64169e9a720
YanovskyOr <yanovsky.or@gmail.com>
2018-07-11 17:14:08 +0300

    add beer screen and design

---
+++ app/src/main/java/ory/ofir/beerz/AddBeerActivity.java
@@ -0,0 +1,13 @@
+package ory.ofir.beerz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_beer);
+    }
+}
